3418cf1ba755e73b69022997a2ad50deb98c637b
peposlawa <peposlawa@gmail.com>
2024-08-06 12:32:23 +0100

    ticket 1 completed, ticket 2 started

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,1 +1,7 @@
-export function calculateScrabbleScore(word) {}
+// get the value of A to return 1
+
+export function calculateScrabbleScore(word) {
+    if (word === 'A') {
+        return 1;
+    }
+}
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,7 @@
-import { calculateScrabbleScore } from "./scrabble-score";
+import { calculateScrabbleScore } from "./scrabble-score.js";
+import { test, expect } from "vitest";
+
+// pass the word A into the function and then assign it a value for that A word
+test('A', () => {
+    expect(calculateScrabbleScore('A')).toEqual(1);
+});
